f590e512f81c71c3c4fbfea4d8f4f8f19c2db26d
Thomas Strub <thomstrub@gmail.com>
2021-04-22 16:20:33 -0700

    adds a logger to server.js

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const dotenv = require('dotenv');
 dotenv.config();
 console.log(process.env.MESSAGE_STYLE, "<----")
 
+// logger
+app.use('/', (req, res, next) => {
+    console.log(req.method + ' ' + req.path.toUpperCase() + ' ' + req.ip)
+    next(); 
+})
+
 
 // app.use(express.static(__dirname + "/public"))
 app.use("/public", express.static(__dirname + "/public"));
