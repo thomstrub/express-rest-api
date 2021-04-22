dc09fc8ae0f81a67d350d93a67551d051651f587
Thomas Strub <thomstrub@gmail.com>
2021-04-22 15:14:20 -0700

    adds /json that serves json

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require('express');
 
 const app = express();
 
+// app.use(express.static(__dirname + "/public"))
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get('/home', (req, res) => {
     res.send('<h1>Welcome Home</h1>');
 })
+
+app.get('/json', (req, res) => {
+    res.json({"message": "hello JSON",
+              "this is also": {"json": "more"}
+    });
+});
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
