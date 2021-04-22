55f634d67ed16f13b85c6321d3ed14dca0411096
Thomas Strub <thomstrub@gmail.com>
2021-04-22 16:33:48 -0700

    adds time logger

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.use('/', (req, res, next) => {
     next(); 
 })
 
+// time server
+
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, function(req, res){
+    res.json({"time": req.time});
+})
 
 // app.use(express.static(__dirname + "/public"))
 app.use("/public", express.static(__dirname + "/public"));
